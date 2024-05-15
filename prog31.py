2dad26143f49f5d61b175546d9c1c7570b1fb28f
ArathiBalachandran <165983084+ArathiBalachandran@users.noreply.github.com>
2024-05-15 11:55:24 +0530

    Update prog31.py

--- prog31.py
+++ prog31.py
@@ -1,9 +1,1 @@
-def sum(b, d):
-    print("Addition:",b+d)
-    print("subtraction:",b-d)
-    print("Multiply:",b*d)
-    print("Division:",b/d)
-
-a = int(input('Enter 1st number: '))
-c = int(input('Enter 2nd number: '))
-sum(a,c)
+print('hello world')
