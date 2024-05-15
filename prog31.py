7c9621a30f2d4b27c0ccdb973bbb85f0fa9c7ac5
ArathiBalachandran <165983084+ArathiBalachandran@users.noreply.github.com>
2024-05-15 11:22:25 +0530

    python program
    hi

---
+++ prog31.py
@@ -0,0 +1,9 @@
+def sum(b, d):
+    print("Addition:",b+d)
+    print("subtraction:",b-d)
+    print("Multiply:",b*d)
+    print("Division:",b/d)
+
+a = int(input('Enter 1st number: '))
+c = int(input('Enter 2nd number: '))
+sum(a,c)
